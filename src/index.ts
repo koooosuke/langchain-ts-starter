bda772743a8a787905456edda6d76cff960499eb
koooosuke <maeda.kosuke2507@gmail.com>
2023-05-11 08:18:28 +0900

    Use streaming on chat model

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,36 @@
-import { AgentExecutor, ChatAgent } from "langchain/agents";
+import { CallbackManager } from "langchain/callbacks";
 import { ChatOpenAI } from "langchain/chat_models";
-import { SerpAPI } from "langchain/tools";
+import { HumanChatMessage } from "langchain/schema";
 
-// Define the list of tools the agent can use
-const tools = [
-  new SerpAPI(process.env.SERPAPI_API_KEY, {
-    location: "Austin,Texas,United States",
-    hl: "en",
-    gl: "us",
+const chat = new ChatOpenAI({
+  streaming: true,
+  callbackManager: CallbackManager.fromHandlers({
+    handleLLMNewToken: async (token) => {
+      process.stdout.write(token);
+    },
   }),
-];
-// Create the agent from the chat model and the tools
-const agent = ChatAgent.fromLLMAndTools(new ChatOpenAI(), tools);
-// Create an executor, which calls to the agent until an answer is found
-const executor = AgentExecutor.fromAgentAndTools({ agent, tools });
+});
 
-const responseG = await executor.run(
-  "How many people live in canada as of 2023?"
-);
+await chat.call([
+  new HumanChatMessage("Write me a song about sparkling water."),
+]);
+/*
+Verse 1:
+Bubbles rise, crisp and clear
+Refreshing taste that brings us cheer
+Sparkling water, so light and pure
+Quenches our thirst, it's always secure
 
-console.log(responseG);
+Chorus:
+Sparkling water, oh how we love
+Its fizzy bubbles and grace above
+It's the perfect drink, anytime, anyplace
+Refreshing as it gives us a taste
+
+Verse 2:
+From morning brunch to evening feast
+It's the perfect drink for a treat
+A sip of it brings a smile so bright
+Our thirst is quenched in just one sip so light
+...
+*/
